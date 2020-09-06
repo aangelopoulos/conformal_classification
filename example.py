098270172429df0409ac61bfc36ba22ad3d78b6e
aa <angelopoulos@berkeley.edu>
2020-09-06 13:50:58 -0700

    [reorganize] main is minimal.

--- example.py
+++ example.py
@@ -5,7 +5,7 @@ import torchvision.transforms as transforms
 import numpy as np
 import matplotlib.pyplot as plt 
 import time
-from utils import AverageMeter
+from utils import * 
 from conformal import ConformalModel
 import torch.backends.cudnn as cudnn
 import pdb
@@ -14,68 +14,6 @@ parser = argparse.ArgumentParser(description='Conformalize Torchvision Model on 
 parser.add_argument('data', metavar='DIR', help='path to Imagenet Val')
 parser.add_argument('--batch_size', metavar='BSZ', help='batch size', default=128)
 parser.add_argument('--num_workers', metavar='NW', help='number of workers', default=0)
-parser.add_argument('--print_freq', metavar='PF', help='print frequency', default=1)
-
-def validate(val_loader, model, criterion):
-    with torch.no_grad():
-        batch_time = AverageMeter('batch_time')
-        losses = AverageMeter('losses')
-        top1 = AverageMeter('top1')
-        top5 = AverageMeter('top5')
-        coverage = AverageMeter('RAPS coverage')
-        size = AverageMeter('RAPS size')
-        # switch to evaluate mode
-        model.eval()
-        end = time.time()
-        for i, (x, target) in enumerate(val_loader):
-            target = target.cuda()
-            # compute output
-            output, S = model(x.cuda())
-            loss = criterion(output, target)
-            # measure accuracy and record loss
-            prec1, prec5 = accuracy(output, target, topk=(1, 5))
-            cvg, sz = coverage_size(S, target)
-
-            # Update meters
-            losses.update(loss.item(), n=x.shape[0])
-            top1.update(prec1.item(), n=x.shape[0])
-            top5.update(prec5.item(), n=x.shape[0])
-            coverage.update(cvg, n=x.shape[0])
-            size.update(sz, n=x.shape[0])
-
-            # measure elapsed time
-            batch_time.update(time.time() - end)
-            end = time.time()
-            if i % args.print_freq == 0:
-                print(f'\rN: {i*x.shape[0]} | Time: {batch_time.val:.3f} ({batch_time.avg:.3f}) | Loss: {losses.val:.4f} ({losses.avg:.4f}) | Cvg@1: {top1.val:.3f} ({top1.avg:.3f}) | Cvg@5: {top5.val:.3f} ({top5.avg:.3f}) | Cvg@RAPS: {coverage.val:.3f} ({coverage.avg:.3f}) | Size@RAPS: {size.val:.3f} ({size.avg:.3f})', end='')
-    print('') #Endline
-
-    return top1.avg
-
-def coverage_size(S,targets):
-    covered = 0
-    size = 0
-    for i in range(targets.shape[0]):
-        if (targets[i].item() in S[i]):
-            covered += 1
-        size = size + S[i].shape[0]
-    return float(covered)/targets.shape[0], size/targets.shape[0]
-
-def accuracy(output, target, topk=(1,)):
-    """Computes the precision@k for the specified values of k"""
-    maxk = max(topk)
-    batch_size = target.size(0)
-
-    _, pred = output.topk(maxk, 1, True, True)
-    pred = pred.t()
-    correct = pred.eq(target.view(1, -1).expand_as(pred))
-
-    res = []
-    for k in topk:
-        correct_k = correct[:k].view(-1).float().sum(0)
-        res.append(correct_k.mul_(100.0 / batch_size))
-    return res
-
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -90,7 +28,7 @@ if __name__ == "__main__":
                 ])
 
     # Get the conformal calibration dataset
-    num_calib = 10000 
+    num_calib = 20000 
     imagenet_calib_data, imagenet_val_data = torch.utils.data.random_split(torchvision.datasets.ImageFolder(args.data, transform), [num_calib,50000-num_calib])
 
     # Initialize loaders 
@@ -100,7 +38,7 @@ if __name__ == "__main__":
     criterion = torch.nn.CrossEntropyLoss().cuda()
     cudnn.benchmark = True
 
-    model = torch.nn.DataParallel(torchvision.models.resnet18(pretrained=True,progress=True)).cuda() 
+    model = torch.nn.DataParallel(torchvision.models.resnet152(pretrained=True,progress=True)).cuda() 
     # Conformalize model
     model = ConformalModel(model, calib_loader, alpha=0.1, kreg=4, lamda=100)
 
